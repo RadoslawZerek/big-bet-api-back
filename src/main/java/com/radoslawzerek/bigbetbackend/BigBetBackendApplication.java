bfb035ddeec919e9891c519bdf835a8c57fae763
Radoslaw <zerek.r93@gmail.com>
2021-08-04 22:50:59 +0200

    Initial commit / added configuration in app.properties and added dependencies in gradle

---
+++ src/main/java/com/radoslawzerek/bigbetbackend/BigBetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.radoslawzerek.bigbetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigBetBackendApplication.class, args);
+    }
+
+}
